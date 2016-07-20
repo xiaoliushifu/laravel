cef9de29070a9f59bee1eb873f0652d82e813d26
liumingwei <872140945@qq.com>
2016-07-20 11:13:06 +0800

    没啥说的,授课计划测试

---
+++ newfile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>后台登录 - 安虫平台</title>
+</head>
+<body>
+<form  action="http://admin.accc.net/user/smsauth" method="post">
+<input name="guid" type="text"  value="0" />
+<input id="" name="action" type="text" value='2' />
+<input id="" name="param" type="text" value='{"phone" : "13581968973","action" : 2}' />
+<input type="submit" value="提交" />
+</form>
+</body>
+</html>
